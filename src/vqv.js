4bc2d73922fd67e7823c7bcc240d4eceace006dd
Caio <thehaseol@gmail.com>
2021-03-26 16:57:24 -0300

    tentativa do requisito student

--- src/vqv.js
+++ src/vqv.js
@@ -16,18 +16,6 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {
-  let mensagem;
-  if (!nome || !idade) {
-    return undefined;
-  }
-
-  mensagem = (
-    `Oi, meu nome é ${nome}!
-Tenho ${idade} anos,
-trabalho na Trybe e mando muito em programação!
-#VQV!`);
-  return mensagem;
-};
+const vqv = (nome, idade) => {};
 
 module.exports = vqv;
