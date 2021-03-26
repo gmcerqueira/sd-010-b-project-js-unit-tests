4b23a9586c32ef191873a0fd9c379557a3e8f43a
Jessie <miss_sc_jessi@hotmail.com>
2021-03-26 19:23:41 -0300

    arrumando lint

--- src/myCounter.js
+++ src/myCounter.js
@@ -21,11 +21,12 @@ const myCounter = () => {
     myArray.push(counter);
     for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
       myArray.push(counter);
-    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter2);
+      for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+        myArray.push(counter2);
+      }
     }
+    return myArray;
   }
-  return myArray;
 };
 
 module.exports = myCounter;
