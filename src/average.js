0d85bcffa9a642e0ebafa6aebb944e7fdefb0cc9
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-03-26 15:30:27 -0300

    requisito average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let soma = 0;
+  let media = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let key = 0; key < array.length; key += 1){
+    const elemento = array[key];
+    if (typeof elemento !== 'number'){
+      return undefined;
+    }
+    soma += elemento;
+  }
+  media = soma / array.length;
+  return Math.round(media);
+};
 
 module.exports = average;
