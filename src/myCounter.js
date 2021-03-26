801fc93a30e413952c1fc19b4d0886560aa48751
Caio <thehaseol@gmail.com>
2021-03-26 19:09:14 -0300

    Fix myCount

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,16 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (let secondCounter = 2; secondCounter <= 3; secondCounter += 1) {
-      myArray.push(counter);
+  var myArray = [];
+  for (var counter1 = 0; counter1 <= 3; counter1 += 1) {
+    myArray.push(counter1);
+    for (var counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
 };
+
+module.exports = myCounter;
 
 module.exports = myCounter;
