1730b8bb30bd67a32431747d98fe76103c7502c3
Camila <camilafabiabh9@gmail.com>
2021-03-26 18:14:07 -0300

    atualizando

--- src/vqv.js
+++ src/vqv.js
@@ -17,14 +17,6 @@
 */
 
 const vqv = (nome, idade) => {
-  if(nome !== ' ' &&  !idade){
-    return undefined
-  } else {
-    return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
-  }
 };
 console.log(vqv(Tunico, 30));
 
