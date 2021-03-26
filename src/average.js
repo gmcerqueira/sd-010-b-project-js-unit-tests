1eed7c90fe547842105cb5f2f743d605ff7ae148
Fernando <fhomaia@gmail.com>
2021-03-26 15:27:01 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -18,7 +18,7 @@ const average = (array) => {
   }
   let media = 0;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof(array[index]) !== number) {
+    if (typeof(array[index]) !== 'number') {
       return undefined;
     } else {
       media += array[index];
