e09562d7d03e5eb0be08cd84ceff53ca1426129a
Abimael <abimaelr.albu@gmail.com>
2021-03-26 18:18:17 -0300

    Teste do lint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,9 +23,16 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+// eslint-disable-next-line max-lines-per-function
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     assert.strictEqual(circle(),undefined);
+
+    assert.strictEqual(() => {
+      let value = circle(3);
+      value['area'] = parseFloat(value['area']).toPrecision(4);
+      return value;
+    }, {radius: 3, area: 28.26, circumference: 18.84});
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
