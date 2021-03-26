76d2f565c320a4fa8b80a76c2be1ab6d76be9ed8
legalvao1 <legalvao1@gmail.com>
2021-03-26 18:19:20 -0300

    Requisito 5 - Funçao CreateStudent

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,6 +35,7 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.deepStrictEqual(circle(2).circumference, 2 * 3.14 * 2);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.deepStrictEqual(circle(3).area, 3.14 * 3 * 3);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
