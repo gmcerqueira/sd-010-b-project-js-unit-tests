b36f85c746182ce082093b8f8e27c4e23a42bbf4
Fabio Marturano <fabiocmjor@gmail.com>
2021-03-26 17:07:12 -0300

    Requisito Circle

--- tests/createStudent.spec.js
+++ tests/createStudent.spec.js
@@ -28,11 +28,13 @@ describe('#createStudent', () => {
     assert.strictEqual(typeof estudante.feedback, 'function');
     assert.strictEqual(estudante.name, 'Leandrão, o Lobo Solitário');
     assert.strictEqual(estudante.feedback(), 'Eita pessoa boa!');
+
     const estudante2 = createStudent('Nobre');
     assert.strictEqual(typeof estudante2, 'object');
     assert.strictEqual(typeof estudante2.feedback, 'function');
     assert.strictEqual(estudante2.name, 'Nobre');
     assert.strictEqual(estudante2.feedback(), 'Eita pessoa boa!');
+
     const estudante3 = createStudent('Inácio');
     assert.strictEqual(typeof estudante3, 'object');
     assert.strictEqual(typeof estudante3.feedback, 'function');
