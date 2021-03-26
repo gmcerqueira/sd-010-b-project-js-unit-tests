89f493e72e0730b9f0e7c8a0bcd392b229ded467
root <diegoserafim1@gmail.com>
2021-03-26 19:04:51 -0300

    Resolvendo erro de excesso de linhas no vqv e restaurant

--- tests/vqv.spec.js
+++ tests/vqv.spec.js
@@ -28,25 +28,16 @@ describe('#vqv', () => {
     assert.strictEqual(typeof vqv, 'function');
     assert.strictEqual(typeof vqv('Tunico', 30), 'string');
     assert.strictEqual(
-      vqv('Tunico', 29),
-      `${'Oi, meu nome é Tunico!\n'
-        + 'Tenho 29 anos,\n'}${
-        workMessage
-      }#VQV!`,
+      vqv('Tunico', 29), `${'Oi, meu nome é Tunico!\n'
+      + 'Tenho 29 anos,\n'}${workMessage}#VQV!`,
     );
     assert.strictEqual(
-      vqv('Alberto', 30),
-      `${'Oi, meu nome é Alberto!\n'
-        + 'Tenho 30 anos,\n'}${
-        workMessage
-      }#VQV!`,
+      vqv('Alberto', 30), `${'Oi, meu nome é Alberto!\n'
+      + 'Tenho 30 anos,\n'}${workMessage}#VQV!`,
     );
     assert.strictEqual(
-      vqv('Hamaji', 29),
-      `${'Oi, meu nome é Hamaji!\n'
-        + 'Tenho 29 anos,\n'}${
-        workMessage
-      }#VQV!`,
+      vqv('Hamaji', 29), `${'Oi, meu nome é Hamaji!\n'
+      + 'Tenho 29 anos,\n'}${workMessage}#VQV!`,
     );
     assert.strictEqual(vqv(), undefined);
   });
