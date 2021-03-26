e8aaba826713c6a19286bd02c0658acc062d90e7
Felippe Correa <felippecorrea2010@gmail.com>
2021-03-26 17:51:35 -0300

    ADicionando requisito 2

--- src/average.js
+++ src/average.js
@@ -15,15 +15,17 @@ const average = (arr) => {
   if (arr.length === 0) {
     return undefined;
   }
-  for(let index = 0; index < arr.length; index += 1){
-    if (typeof(arr[index]) !== 'number') {
+  for(let index = 0; index < arr.length; index += 1) {
+    if (typeof (arr[index]) !== 'number') {
       return undefined;
-    };
-  };
+    }
+  }
+
   let result = 0;
   for (let index = 0; index < arr.length; index += 1) {
-    result += arr[index];
+    result += arr[index]
   };
+
   return Math.round(result / arr.length);
 };
 
