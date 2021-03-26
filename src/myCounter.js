51339c747e3080006ca0667507c925494726808e
root <diegoserafim1@gmail.com>
2021-03-26 18:53:37 -0300

    resolvendo erro de conflito no mycount

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let count = 2; count <= 3; count += 1) {
-      myArray.push(count);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
