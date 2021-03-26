257bb35ea65d5cb1d12b67fa030101b3c03ea207
Jessie <miss_sc_jessi@hotmail.com>
2021-03-26 19:46:55 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,13 +20,10 @@ const myCounter = () => {
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
-      myArray.push(counter);
-      for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-        myArray.push(counter2);
-      }
+      myArray.push(counter1);
     }
-    return myArray;
   }
+  return myArray;
 };
 
 module.exports = myCounter;
