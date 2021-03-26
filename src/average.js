7e7db09d575021ed9207197a4fb687f3bf03912a
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-26 16:54:45 -0300

    Adiciona a funcao average (requisito 1)

--- src/average.js
+++ src/average.js
@@ -12,6 +12,22 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  // add your implementation here
+  if (array.length > 0) {
+    let soma = 0;
+    let media = 0;
+    for (let i = 0; i < array.length; i += 1) {
+      if (typeof (array[i]) !== 'number') {
+        return undefined;
+      }
+      soma += array[i];
+      media = Math.round(soma / array.length);
+    }
+    return media;
+  }
+  return undefined;
+};
+
 
 module.exports = average;
