faddc516767a733ebd725ebcf197f4f4ad6ba79c
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-03-26 16:43:29 -0300

    Mudanças no arquivo para o lint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
-const assert = require("assert");
-const circle = require("../src/circle");
+const assert = require('assert');
+const circle = require('../src/circle');
 
 /*
   Essa função recebe o raio de um círculo e retorna um objeto contendo suas informações (Raio, Área e Circunferência).
@@ -23,12 +23,12 @@ const circle = require("../src/circle");
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe("#circle", () => {
-  it("given a radius, should return an object with circles info", () => {
+describe('#circle', () => {
+  it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
 
     // Teste se circle retorna um objeto.
-    assert.strictEqual(typeof circle(1), "object");
+    assert.strictEqual(typeof circle(1), 'object');
 
     // Teste se o objeto retornado tem 3 entradas.
     assert.strictEqual(Object.keys(circle(1)).length, 3);
