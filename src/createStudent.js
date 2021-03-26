9b383fce02d30102d921e0fc94dfe917c7ff1b64
hzozimo <hzozimo@gmail.com>
2021-03-26 18:10:05 -0300

    exercicio 5 continua sendo feito, push para verificaçao do avaliador online

--- src/createStudent.js
+++ src/createStudent.js
@@ -17,7 +17,7 @@
 
 const createStudent = (name) => {
 
-  return {name: `${name}`, feedback: }
+  return {name: `${name}`, feedback: () => 'Eita pessoa boa!'}
 };
 
 module.exports = createStudent;
