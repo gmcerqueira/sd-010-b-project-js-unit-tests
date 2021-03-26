c4fd8f8cd3b5ff2c95036dac413ad24d7e5f07f2
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-26 17:56:05 -0300

    Inicio Requisito 9

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -51,11 +51,12 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // TESTE 1: Verifique se o retorno da função createMenu() é um objeto que possui, 
-    // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.
+    // mas não é necessariamente é limitado à chave `fetchMenu`, a qual tem como valor uma função.    
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
+    assert.deepStrictEqual(typeof createMenu(), 'object');
     // ```
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`, 
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
@@ -113,5 +114,6 @@ describe('#createMenu', () => {
     // objetoRetornado.pay() // Retorno: somaDosPreçosDosPedidos
     // ```
     // Agora faça o PASSO 4 no arquivo `src/restaurant.js`.
+    
   });
 });
