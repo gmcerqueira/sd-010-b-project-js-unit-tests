4cdf65563de3ff370c791684c58cc828cb8d02f1
Lara Capila <laracapilak@gmail.com>
2021-03-26 16:41:12 -0300

    função VQV - requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -20,9 +20,9 @@ const vqv = (nome, idade) => {
   if (nome.length === 0 || idade.length === 0) {
     return undefined;
   }
-    return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
 };
 module.exports = vqv;
