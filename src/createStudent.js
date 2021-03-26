bbb7058d952f43587a618957c365736415a10c7e
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-03-26 18:40:57 -0300

    Commit para dar o último Pull para atualizar conflitos

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (string) => {
+  const estudante = {
+    name: 'nome',
+    feedBack: () => 'Eita pessoa boa!',
+  }
+  estudante.name = string;
+  return estudante
+};
 
 module.exports = createStudent;
