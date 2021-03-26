68b8f3221fd815e522e6e9153d04f35d9423dc39
root <diegoserafim1@gmail.com>
2021-03-26 18:14:55 -0300

    Erro pc nao executa npm

--- src/average.js
+++ src/average.js
@@ -12,16 +12,32 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (array) => {
+const average = () => {
   // iniciando o projeto em 26/03
-  if (array !== 'number' || array === 0) {
+  if (average !== 'number' || average === 0) {
     return undefined;
   }
   let resultado = 0;
-  for (let index = 0; index <= array; index += 1) {
-    resultado += array[index];
+  for (let index = 0; index <= average; index += 1) {
+    resultado += average[index];
   }
-  return Math.floor(resultado / array.length);
+  return Math.floor(resultado / average.length);
 };
+    assert.strictEqual(average([3, 4, 5]), 4);
+    assert.strictEqual(average([1, 2, 3, '4', 5]), undefined);
+    assert.strictEqual(average([0, 0, 0, 0, 0, 0, 0]), 0);
+    assert.strictEqual(average([1, 2, '3']), undefined);
+    assert.strictEqual(average([1, 2, 3]), 2);
+    assert.strictEqual(average([0, 0, 0, 0, 0, 0, 1]), 0);
+
+    assert.strictEqual(average([]), undefined);
+    assert.strictEqual(average([' ']), undefined);
+    assert.strictEqual(average(['um', 'dois', 'tres']), undefined);
+    assert.strictEqual(average([47, 63, 122]), 77);
+
+    assert.strictEqual(average([-11, 2, 5]), -1);
+
+    assert.strictEqual(average([-11, -5, 2]), -5);
 
 module.exports = average;
+console.log(average);
