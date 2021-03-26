7be0c702fd6b3eac9a65eb00ca73ee3fa919ae21
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-26 18:22:02 -0300

    circle

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,10 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = () => {
+  let saida = { name: `${name}`, feedback: () => ('Eita pessoa boa!')}
+  
+  return saida;
+};
 
 module.exports = createStudent;
