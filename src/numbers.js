53b81349f5d74bc68706aed1b21cf95ac4129146
Carlos Vieira <appencigr@gmail.com>
2021-03-26 15:20:56 -0300

    Requisito dois feito

--- src/numbers.js
+++ src/numbers.js
@@ -8,7 +8,7 @@
     - numbers([2, 3, 4]); // Retorna: true
     - numbers([2, 'errado', 5]); // Retorna: false
 */
-
+const assert = require('assert');
 const numbers = (myArray) => {
   for (let i = 0; i < myArray.length; i += 1) {
     if (typeof myArray[i] !== 'number') {
@@ -17,5 +17,11 @@ const numbers = (myArray) => {
   }
   return true;
 };
+
+assert.strictEqual(typeof myArray, 'function');
+assert.strictEqual(typeof myArray([2, 3, 4]), true);
+assert.throws(() =>{
+  myArray([2, 'errado', 5]);
+}, /^Error: parameters must be numbers$/);
 
 module.exports = numbers;
