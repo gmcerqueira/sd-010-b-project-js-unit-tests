cd688f2a07b9a901bc11cb1ec5157836b2885b67
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-26 19:29:50 -0300

    tentando a 1 a 2

--- src/vqv.js
+++ src/vqv.js
@@ -16,11 +16,6 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {
-  if (nome === undefined || idade === undefined) { return undefined; } else {
-    return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,
-    \ntrabalho na Trybe e mando muito em programação!\n#VQV!'`;
-  }
-};
+const vqv = (nome, idade) => {};
 
 module.exports = vqv;
