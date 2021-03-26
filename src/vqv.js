648b8f7809586b71eb4ba3bff0e4c08ae43b3068
Camila <camilafabiabh9@gmail.com>
2021-03-26 18:26:33 -0300

    adicionando nova skill

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
+  if(!nome || !idade ) {
+    return undefined;
+  }
+    return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
 };
-console.log(vqv(Tunico, 30));
 
 module.exports = vqv;
