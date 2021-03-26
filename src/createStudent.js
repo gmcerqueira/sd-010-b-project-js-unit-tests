38e1ff686596c00758b7c42d7dbfc18ec03be075
hzozimo <hzozimo@gmail.com>
2021-03-26 18:05:52 -0300

    exercicio 5 sendo feito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+
+  return {name: `${name}`, feedback: }
+};
 
 module.exports = createStudent;
