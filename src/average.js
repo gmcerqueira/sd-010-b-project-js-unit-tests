0ea8fee68ef68e907edb73bbd122efe1736f6a04
Lara Capila <laracapilak@gmail.com>
2021-03-26 16:12:39 -0300

    função average - requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let value = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
 
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    value += array[index];
+  }
+  return Math.round(value / array.length);
+};
 module.exports = average;
