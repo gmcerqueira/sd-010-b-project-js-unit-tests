be51eeef432e2505434bdb8f8e82567900566e8c
Igor Machado <rf59imft@gmail.com>
2021-03-26 17:11:40 -0300

    desafio 04

--- src/circle.js
+++ src/circle.js
@@ -19,13 +19,17 @@
 */
 
 const circle = (radius) => {
-  const PI = 3.14;
-  if (!radius) { return undefined; }
+  const PI = 3.14
+  if (!radius) {
+    return undefined
+  }
   return {
     radius,
     area: PI * radius * radius,
     circumference: 2 * PI * radius,
-  };
-};
+  }
+}
 
-module.exports = circle;
+module.exports = circle
+
+console.log(circle(3))
