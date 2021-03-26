93a00fec7d91bd256e1d73c3f7c317f9004ced93
Antenor <antenorzapata@gmail.com>
2021-03-26 19:32:48 -0300

    lint

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,20 +1,11 @@
-
-const obj = {
-  food: { coxinha: 3.9, sopa: 9.9 },
-  drink: { agua: 3.9, cerveja: 6.9 },
-};
-
 function orderFromMenu(value) {
-
   return this.consumption.push(value);
 }
 
 const createMenu = (obj) => {
   let result = 0;
-
   const menu = {
     fetchMenu: () => obj,
-
     consumption: [],
     order: orderFromMenu,
     pay: function () {
