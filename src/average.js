bd3b17bee8210299f424ff1a0717ba6034d9fc3d
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-26 16:13:22 -0300

    Corrige lint

--- src/average.js
+++ src/average.js
@@ -19,7 +19,7 @@ const average = (array) => {
     return undefined;
   }
   for (let i = 0; i < array.length; i += 1) {
-    if (typeof array[i] !== number) {
+    if (typeof array[i] !== 'number') {
       return undefined;
     }
     soma += array[i];
