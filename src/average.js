5081ff59b80d2d8a95c735ac1053af2333f57432
Letycia Barreto <letyciasbarreto@gmail.com>
2021-03-26 17:07:24 -0300

    requisito 2

--- src/average.js
+++ src/average.js
@@ -13,7 +13,18 @@
 */
 
 const average = (array) => {
-  
+  if (array.length === 0) {
+    return undefined;
+  }
+  let soma = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof (array[i]) !== 'number') {
+      return undefined;
+    }
+    soma += array[i];
+  }
+  let media = soma / array.length;
+  return Math.round(media);
 };
 
 module.exports = average;
