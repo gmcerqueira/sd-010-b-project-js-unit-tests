318c81041baf7b9fd4cd5ca578a0e4730bd8e4aa
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-03-26 17:09:45 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const template = `'Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!'`
+
+  return template;
+};
 
 module.exports = vqv;
