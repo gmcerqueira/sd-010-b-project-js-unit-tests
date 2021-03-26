c2eff45ab25dfa8aec1b3f7d08f1142ee2bccc2b
Antenor <antenorzapata@gmail.com>
2021-03-26 16:37:55 -0300

    product details

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,7 +33,36 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
+    assert.strictEqual(
+      Array.isArray(productDetails('iphone', 'celular')),
+      true
+    );
+
+    // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('iphone', 'celular').length, 2);
+
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('iphone', 'celular'), 'object');
+
+    // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(
+      productDetails('iphone', 'celular')[0],
+      productDetails('iphone', 'celular')[1]
+    );
+
+    // (Difícil) Teste que os dois productIds terminam com 123.
+    const product1 = productDetails(
+      'iphone',
+      'celular'
+    )[0].details.productId.slice(-3);
+
+    const product2 = productDetails(
+      'iphone',
+      'celular'
+    )[1].details.productId.slice(-3);
+
+    assert.strictEqual(product1, product2, '123');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     // Teste que o array retornado pela função contém dois itens dentro.
