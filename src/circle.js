6e171b992b2226e0db619c060b6b446b81e69531
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-26 18:44:38 -0300

    Correçao da funçao circle

--- src/circle.js
+++ src/circle.js
@@ -25,9 +25,9 @@ const circle = (radius) => {
   }
   return {
     radius,
-    area: parseFloat((PI * radius * radius).toPrecision(2)),
-    circumference: parseFloat((2 * PI * radius).toPrecision(2)),
+    area: parseFloat((PI * radius * radius).toFixed(2)),
+    circumference: parseFloat((2 * PI * radius).toFixed(2)),
   };
 };
-
+console.log(circle(3))
 module.exports = circle;
