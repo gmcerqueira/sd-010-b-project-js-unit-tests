7fcb6b2f674ff1f4c5e61c6464f75d141bff91bd
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-26 17:10:04 -0300

    Corrigindo erro lint, requisito 1 e 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -21,11 +21,10 @@ describe('#numbers', () => {
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    assert.deepStrictEqual(numbers(1, 2, '3', 4, 5), false);
+    assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    assert.deepStrictEqual(numbers[1, 2, 'a', 3], false);
+    assert.deepStrictEqual(numbers([1, 2, 'a', 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
-    assert.deepStrictEqual(numbers[' '], false)
+    assert.deepStrictEqual(numbers([' ']), false);
   });
 });
-
