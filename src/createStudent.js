e9f1bcf5ab61cc321a7ce5fbdd9ee0a741165d5a
Johnny-Batata <johnny.arguello007@gmail.com>
2021-03-26 19:49:45 -0300

    requisito 3

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const feedBack = () => 'Eita pessoa boa!';
+
+  return { name: `${name}`, feedback: feedBack };
+};
+
 
 module.exports = createStudent;
