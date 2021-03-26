10fa7032a325aa4a765fecda804aa4a49cf1de45
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-26 19:51:19 -0300

    Mudando requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -13,17 +13,17 @@
       'Oi, meu nome é Tunico!
       Tenho 30 anos,
       trabalho na Trybe e mando muito em programação!
-      #VQV!'s
+      #VQV!'
 */
 
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
+  return `Oi, meu nome é ${nome}!\n`
+    + `Tenho ${idade} anos,\n`
+    + 'trabalho na Trybe e mando muito em programação!\n'
+    + '#VQV!'
 };
 
 module.exports = vqv;
