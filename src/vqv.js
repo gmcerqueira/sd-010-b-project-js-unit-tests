62fed220d0e23c4bebb28cc8b44a5993154e26c1
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-26 18:46:16 -0300

    Corregindo linter

--- src/vqv.js
+++ src/vqv.js
@@ -19,9 +19,8 @@
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
-  } 
-   return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\n` +
-   `trabalho na Trybe e mando muito em programação!\n#VQV!`;
+  } return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\n`
+  + 'trabalho na Trybe e mando muito em programação!\n#VQV!';
 };
 
 module.exports = vqv;
