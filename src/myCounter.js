13d1d92f4515cfb5982911deeb67c87532154f8a
Igor Machado <rf59imft@gmail.com>
2021-03-26 18:13:41 -0300

    desafio 08

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,15 +16,15 @@
 */
 
 const myCounter = () => {
-  let myArray = []
+  let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter)
+    myArray.push(counter);
     for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter)
+      myArray.push(counter);
     }
   }
-  return myArray
-}
+  return myArray;
+};
 
-module.exports = myCounter
-console.log(myCounter([]))
+module.exports = myCounter;
+// console.log(myCounter([]))
