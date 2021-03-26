0e9ec7851b74b71579a219acabf8ea32fe64fa73
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-26 15:14:19 -0300

    add exercicio 1

--- src/average.js
+++ src/average.js
@@ -11,9 +11,25 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
+const assert = require('assert');
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  let sum = 0;
+  let mean = 0;
+
+  if (array.length === 0) {
+    return undefined;
+  }
+  
+  for (let index = 0; index < array.length; index +=1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    } else {
+      sum += array[index];
+      mean = sum / (array.length); 
+    }
+  }
+  return Math.round(mean);
 };
 
 module.exports = average;
