976f44035d496914159e6075d5b5da17fdd21240
Matheus Carretta <matheuscarretta@gmail.com>
2021-03-26 15:31:46 -0300

    Requisito 1 - cria funçao de calcular media

--- src/average.js
+++ src/average.js
@@ -12,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
- 
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let average = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'number') {
+      average += array[index];
+    } else {
+      return undefined;
+    }
+  }
+  average /= array.length;
+  return Math.round(average);
+};
+
+
 module.exports = average;
