99f2855bf2390fdc74cdbb56085254e552cd4256
Fernando <fhomaia@gmail.com>
2021-03-26 16:47:56 -0300

    Requisito 5

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -31,7 +31,7 @@ describe('#circle', () => {
     // Teste se o objeto retornado tem 3 entradas.
     assert.strictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    assert.strictEqual(circle(), 'undefined');
+    assert.deepStrictEqual(circle(), 'undefined');
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     const circunf = circle(2).circumference;
     assert.strictEqual(circunf, 12.56);
