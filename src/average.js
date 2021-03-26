cf111a2f11be856e653cb2f2f1c08da927e7f68f
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-26 16:04:49 -0300

    nova atualização do repo

--- src/average.js
+++ src/average.js
@@ -15,7 +15,7 @@
 const average = (arr) => {
   total = 0;
   for (let i = 0; i <= arr.length; i += 1) {
-    if (typeof arr !== 'number') {
+    if (typeof arr[i] !== 'number') {
       return undefined;
     }else {
       total += arr[i];
@@ -23,5 +23,7 @@ const average = (arr) => {
   }
   return total/arr.length;
 };
+
+console.log(average([3, 4, 5]));
 
 module.exports = average;
