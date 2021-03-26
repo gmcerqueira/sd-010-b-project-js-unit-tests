f2fd5c6794bdafb4ed8704b67f61e91502c12cc4
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-26 18:03:51 -0300

    add exercicio 6

--- src/vqv.js
+++ src/vqv.js
@@ -16,19 +16,16 @@
       #VQV!'
 */
 
-
 const vqv = (nome, idade) => {
-
   if (nome == null && idade == null) {
     return undefined;
   }
 
-  const phrase =
-  `Oi, meu nome é ${nome}!
+  const phrase =  `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
-  return phrase
+  return phrase;
 };
 
 module.exports = vqv;
