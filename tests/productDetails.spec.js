d10141ceb48552d5a761540af24a4d6e8cf2ff41
Fernando <fhomaia@gmail.com>
2021-03-26 18:38:50 -0300

    Requisitos 3,4 e 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -6,7 +6,7 @@ const productDetails = require('../src/productDetails');
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
-
+ .
   Parâmetros:
   - Uma string;
   - Uma string;
