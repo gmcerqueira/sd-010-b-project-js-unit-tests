fb97abb90eb88d180aab1c9ae43ad60698ad520f
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-03-26 19:19:13 -0300

    refatoraçao de tudo para teste

--- src/average.js
+++ src/average.js
@@ -18,9 +18,9 @@ const average = (array) => {
   }
   let sum = 0;
   for (index = 0; index < array.length; index += 1) {
-    if (typeof array[index] !== "number") {
+    if (typeof array[index] !== 'number') {
       return undefined;
-    } 
+    }
     sum += array[index];
   }
   return Math.round(sum / array.length);
