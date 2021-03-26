d9a528fc9ed09ad8931435fe95fcc798a955ae43
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-26 17:01:51 -0300

    createStudent done

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,16 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (string) => {
+
+  const feedback = () => {
+    return 'Eita pessoa boa!';
+  }
+
+  return {
+    name: string,
+    feedback,
+  };
+};
 
 module.exports = createStudent;
