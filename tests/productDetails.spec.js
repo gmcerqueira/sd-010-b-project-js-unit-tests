32e23e04893ea0ff14b6627d08d1d873d6074018
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-03-26 23:39:39 -0300

    req6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -40,8 +40,16 @@ describe('#productDetails', () => {
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(Object.keys(productDetails('blabla', 'blublu')).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // blaablalbal
+    assert.strictEqual(typeof (Object.keys(productDetails('blabla', 'blublu')[0])), 'object');
+    assert.strictEqual(typeof (Object.keys(productDetails('blabla', 'blublu')[1])), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    let ar = productDetails('blabla', 'blabla');
+    assert.notDeepStrictEqual(Object.keys(ar)[0], Object.keys(ar)[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    let ar2 = [];
+    ar2[0] = productDetails('blabla', 'blublu')[0].details.productId.endsWith('123');
+    ar2[1] = productDetails('blabla', 'blublu')[1].details.productId.endsWith('123');
+    assert.strictEqual(ar2[0], true);
+    assert.strictEqual(ar2[1], true);
   });
 });
