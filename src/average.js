d77c2e6fa26201dde82ae75b9cfc7fb2907b1cbd
BarrisennLinux <nuncaparedelutar1984@gmail.com>
2021-03-27 19:12:41 -0300

    Correção arquivo src/avarege

--- src/average.js
+++ src/average.js
@@ -1,7 +1,6 @@
 /*
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
-  Caso a função receba algum valor não numérico ou um array vazio,
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
   Parâmetros:
@@ -12,18 +11,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-const average = (parameter) => {
-  const array = parameter;
-  let sum = 0;
-  if (!array || array.length === 0) { return undefined; }
-  for (let element of array) {
-    if (typeof (element) !== 'number') {
-      return undefined;
-    }
-    sum += element;
-  }
-  return Math.round(sum / array.length);
+const average = (array) => {
+  let count = 0;
+  let result = 0;
+  if (array.length === 0) return undefined;
+  if (array.some((elem) => typeof elem !== 'number')) return undefined;
+  array.forEach((elem) => (result += elem));
+  return Math.round(result / array.length);
+  array.forEach((elem) => {
+    result += elem;
+    count += 1;
+  });
+  return Math.round(result / count);
 };
 
 module.exports = average;
