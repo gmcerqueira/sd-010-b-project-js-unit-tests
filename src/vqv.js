202dad628e61763b1ec6a27621a725ffabf3affe
renatogsouza <renatomacedo1995@gmail.com>
2021-03-27 19:45:45 -0300

    correçao vqv

--- src/vqv.js
+++ src/vqv.js
@@ -20,10 +20,11 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  return (`Oi, meu nome é ${nome}!\n
-  Tenho ${idade} anos,\n
-  trabalho na Trybe e mando muito em programação!
-  \n#VQV!`);
+  return (
+`Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`);
 };
 
 module.exports = vqv;
