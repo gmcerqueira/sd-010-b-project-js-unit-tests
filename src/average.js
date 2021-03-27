5f434ecd03f8953235533378070f97797e8495db
Matheus Bodra <matheusbodra@gmail.com>
2021-03-27 11:15:36 -0300

    requisito 1 correcao

--- src/average.js
+++ src/average.js
@@ -13,12 +13,9 @@
 */
 
 const verificaArray = (array) => {
-  // Verifico se a array não é vazia.
   if (array.length === 0) {
     return undefined;
   }
-
-  // Verifico se todos os valores inseridos são números.
   for (let i = 0; i < array.length; i += 1) {
     if (typeof array[i] !== 'number') {
       return undefined;
@@ -27,9 +24,6 @@ const verificaArray = (array) => {
 };
 
 const average = (array) => {
-  if (verificaArray(array) === undefined) {
-    return undefined;
-  }
   // Faço as operações matematicas para obter a média.
   let mediaAritmetica;
   let somaTotal = 0;
