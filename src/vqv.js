108570c6907242b08b06ec24d0b14b2450554643
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-03-27 04:22:54 -0300

    requisitos

--- src/vqv.js
+++ src/vqv.js
@@ -1,21 +1,11 @@
-/* eslint-disable no-unused-vars */
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  const phrase = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  return phrase;
+};
 
-/*
-  Use template literals para escrever uma função que,
-  recebe seu nome e sua idade e retorna o parágrafo descrito abaixo.
-  Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
-
-  Parâmetros:
-    - Uma string;
-    - Um número.
-  Comportamento:
-    vqv(Tunico, 30) // Retorna:
-      'Oi, meu nome é Tunico!
-      Tenho 30 anos,
-      trabalho na Trybe e mando muito em programação!
-      #VQV!'
-*/
-
-const vqv = (nome, idade) => {};
+console.log(vqv());
 
 module.exports = vqv;
