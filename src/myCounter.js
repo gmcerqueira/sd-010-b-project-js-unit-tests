48b36b866eb7cd5c6a3e374e8aa2a8f5516026e5
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-27 20:39:18 -0300

    Requisito myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -12,14 +12,13 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 3; counter <= 1; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
   return myArray;
 };
 
-console.log(myCounter([0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3]));
-// module.exports = myCounter;
+module.exports = myCounter;
