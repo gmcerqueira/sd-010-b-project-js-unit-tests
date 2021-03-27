5ea305b6e66282d8c80f60f24b77b192eae5b86c
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-27 19:30:39 -0300

    Corrigindo erro lint requisito circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -40,7 +40,7 @@ describe('#circle', () => {
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), {
       radius: 3,
-      area: 28.259999999999998, circumference: 18.84
+      area: 28.259999999999998, circumference: 18.84,
     });
   });
 });
