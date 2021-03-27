d89d453d6dbde5903927e17b1d776848d0999d0a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-26 22:34:22 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (name) => {
+  const student = {
+    Name: name,
+    Feedback: 'Eita pessoa boa'
+  }
+  return student;
+};
 module.exports = createStudent;
