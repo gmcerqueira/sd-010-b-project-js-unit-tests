a450cbd8732495bc128db8d0e0a0947e92a48fd1
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-03-27 04:36:06 -0300

    Em objCalculator.js: implementa um objeto calculator que possui quatro chaves: add; mult; div; sub. Para cada uma delas atribua uma função que realiza a respectiva operação ao receber dois inteiros e retornar um inteiro. Os resultados das divisões devem sempre ser arredondados para baixo.

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -18,7 +18,31 @@
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
+/*
+  https://www.w3schools.com/jsref/jsref_floor.asp
+ */
+const calculator = {
+  add(integer1, integer2) {
+    // retorna a soma de integer1 e integer2
+    return integer1 + integer2;
+  },
+  mult(integer1, integer2) {
+    // retorna a multiplicação de integer1 por integer2
+    return integer1 * integer2;
+  },
+  div(integer1, integer2) {
+    // dispara erro caso divisor (integer2) seja igual a zero
+    if (integer2 === 0) throw new Error('parameter integer2 must not be 0');
 
-const calculator = {};
+    // realiza da divisão e armazena o resultado em result
+    let result = integer1 / integer2;
+
+    return Math.floor(result); // arredonda resultado para baixo
+  },
+  sub(integer1, integer2) {
+    // retorna a subtração de integer1 por integer2
+    return integer1 - integer2;
+  },
+};
 
 module.exports = calculator;
