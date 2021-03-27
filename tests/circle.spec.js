cac93428ef348c919c881abb273aba23e3862e25
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-27 09:07:20 -0300

    começando o restaurante, Jesus

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,7 +27,8 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.notStrictEqual((typeof circle), 'object');
+    let typeCircle = circle(1);
+    assert.strictEqual((typeof typeCircle), 'object');
     // Teste se o objeto retornado tem 3 entradas.
     assert.deepStrictEqual((Object.keys(circle(2)).length), 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,7 +34,8 @@ const productDetails = require('../src/productDetails');
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // Teste que o retorno da função é um array.
-    assert.notDeepStrictEqual(typeof productDetails(), 'array');
+    const typeProducts = productDetails('Alcoolgel', 'Máscara');
+    assert.strictEqual(Array.isArray(typeProducts), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.deepStrictEqual((Object.keys(productDetails()).length), 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
@@ -45,8 +46,8 @@ describe('#productDetails', () => {
     const output = (output1 === output2);
     assert.strictEqual(output, false);
     // (Difícil) Teste que os dois productIds terminam com 123.
-    const outPutAlcool = (productDetails('Alcoolgel', 'Máscara')[0].details.productId);
-    const outPutMascara = (productDetails('Alcoolgel', 'Máscara')[1].details.productId);
+    const outPutAlcool = (productDetails('Alcool gel', 'Máscara')[0].details.productId);
+    const outPutMascara = (productDetails('Alcool gel', 'Máscara')[1].details.productId);
     assert.deepStrictEqual(outPutAlcool.endsWith('123'), true);
     assert.deepStrictEqual(outPutMascara.endsWith('123'), true);
   });
