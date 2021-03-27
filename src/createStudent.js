1d538e830ffbd0d44ad8091947f6c00d584e930e
BarrisennLinux <nuncaparedelutar1984@gmail.com>
2021-03-27 18:20:56 -0300

    Commit arquivo  createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -3,18 +3,22 @@
   retorne um objeto que contenha duas chaves:
     (1) name, contendo o nome passado como parâmetro;
     (2) feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
-
   Faça a função da chave feedback com arrow functions!
-
   Parâmetros:
     - Uma string;
   Comportamento:
     const estudante = createStudent('Leandrão, o Lobo Solitário')
-
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
 const createStudent = () => {};
+const createStudent = (student) => {
+  const result = {
+    name: `${student}`,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return result;
+};
 
 module.exports = createStudent;
