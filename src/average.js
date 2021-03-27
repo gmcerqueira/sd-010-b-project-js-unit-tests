ff2a849b8d5ee60e02ac4754523893fe16825452
BarrisennLinux <nuncaparedelutar1984@gmail.com>
2021-03-27 18:15:16 -0300

    Commit iniciando o projeto js unit tests

--- src/average.js
+++ src/average.js
@@ -1,9 +1,9 @@
 /*
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
+  Caso a função receba algum valor não numérico ou um array vazio,
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
-
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
@@ -13,5 +13,17 @@
 */
 
 const average = () => {};
+const average = (parameter) => {
+  const array = parameter;
+  let sum = 0;
+  if (!array || array.length === 0) { return undefined; }
+  for (let element of array) {
+    if (typeof (element) !== 'number') {
+      return undefined;
+    }
+    sum += element;
+  }
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
