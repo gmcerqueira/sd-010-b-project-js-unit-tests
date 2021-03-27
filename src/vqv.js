591faa934628bc609e325d1de0376fdd1c0e5397
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-27 19:28:43 -0300

    ghj

--- src/vqv.js
+++ src/vqv.js
@@ -17,14 +17,14 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === undefined || idade === undefined) {
+  if (nome === null || idade === null) {
     return undefined;
   }
   if (nome !== undefined || idade !== undefined) {
     return `Oi, meu nome é ${nome}!
-    \nTenho ${idade} anos,
-    \ntrabalho na Trybe e mando muito em programação!
-    \n#VQV!'`;
+    Tenho ${idade} anos,
+    trabalho na Trybe e mando muito em programação!
+    #VQV!'`;
   }
 };
 
