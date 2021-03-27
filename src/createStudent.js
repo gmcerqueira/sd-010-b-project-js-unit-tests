3b841845c3257f52b8c1465691673ec9e901abb1
joao966 <joaooliveira966@gmail.com>
2021-03-27 19:12:13 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
+const createStudent = (name) => {
   let objeto = {
-    n: nome,
+    name: name,
     feedback: () => {
-      'Eita pessoa boa!';
+      let message = 'Eita pessoa boa!';
+      return message;
     },
   };
   return objeto;
