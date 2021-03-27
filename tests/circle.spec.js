1df95d0881368528e36e10888b24a10626eff6a7
renanpbraga <renanpbraga@gmail.com>
2021-03-26 22:13:31 -0300

    adiciona circles passo 1

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,7 +28,9 @@ describe('#circle', () => {
 
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof (circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.entries(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
