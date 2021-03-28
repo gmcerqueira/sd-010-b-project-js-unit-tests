c6aad774ac37f491400dcb8c04af527a2251ba3f
Thiago Leite <leite.tjs@gmail.com>
2021-03-28 02:54:54 -0300

    consertando o assert.fail() circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,7 +27,7 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     assert.fail();
-    assert.strictEqual(circle(Object))
+    assert.strictEqual(circle(Object));
 
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
