148a9f4d9833452e07f58f68a33dfb0698cc9da2
Adão Benites <adaobjr@gmail.com>
2021-03-28 19:09:24 -0400

    Requisito 9 - Implementar os casos de teste e a função createMenu

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
+  let newArray = [];
+  for (let counter1 = 0; counter1 <= 3; counter1 += 1) {
+    newArray.push(counter1);
     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter2);
+      newArray.push(counter2);
     }
   }
-  return myArray;
+  return newArray;
 };
 
 module.exports = myCounter;
