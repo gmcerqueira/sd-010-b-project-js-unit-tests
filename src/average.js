f6b4d98e364d497a838a374460455b5d40beb017
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-27 22:36:21 -0300

    ajuste lint

--- src/average.js
+++ src/average.js
@@ -15,18 +15,18 @@
 const average = (param) => {
   let sum = 0;
   let count = 0;
-  if(param.length === 0){
-      return undefined
+  if (param.length === 0) {
+    return undefined;
   }
-  for(index in param){
-      sum += param[index]
-      count += 1
-      if(typeof(param[index]) != 'number'){
-          return undefined
-      }
+  for (let index = 0; index < param.length; index += 1) {
+    sum += param[index];
+    count += 1;
+    if (typeof (param[index]) !== 'number') {
+      return undefined;
+    }
   }
-  let media = sum/count
-  return Math.round(media)
+  let media = sum / count;
+  return Math.round(media);
 };
 
 module.exports = average;
