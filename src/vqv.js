882929a61bed7dd52261a6c17e988075329fcb7d
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-28 00:37:15 -0300

    ajuste

--- src/vqv.js
+++ src/vqv.js
@@ -1,10 +1,9 @@
 const vqv = (nome, idade) => {
   if (nome !== undefined && idade !== undefined) {
-    return `
-    Oi, meu é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
+    return `Oi, meu é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   }
   return undefined;
 };
