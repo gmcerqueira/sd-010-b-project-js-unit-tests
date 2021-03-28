7b024797fa7273d972659212f579d72c78835807
Jonathan Souza <souzajonathan570@gmail.com>
2021-03-28 13:24:05 -0300

    item 6 passed

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,15 +30,48 @@ const productDetails = require('../src/productDetails');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
+const expected = [
+  {
+    name: 'Alcool gel',
+    details: {
+      productId: 'Alcool gel123',
+    },
+  },
+  {
+    name: 'Máscara',
+    details: {
+      productId: 'Máscara123',
+    },
+  },
+];
+
+const compareObj = () => {
+  let storeFunction = productDetails('Alcool gel', 'Máscara');
+
+  return storeFunction[0] === storeFunction[1];
+};
+
+let productId1 = expected[0].details.productId;
+let productId2 = expected[1].details.productId;
+const parteString = (productId1, productId2) => {
+  if (productId1.substr(-3) === '123' && productId2.substr(-3)) {
+    return true;
+  }
+  return false;
+};
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
+    assert.strictEqual(compareObj(), false);
+    assert.strictEqual(parteString(productId1, productId2), true);
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    // Teste que o retorno da função é um array.*
+    // Teste que o array retornado pela função contém dois itens dentro.*
+    // Teste que os dois itens dentro do array retornado pela função são objetos.*
+    // Teste que os dois objetos são diferentes entre si.*
+    // (Difícil) Teste que os dois productIds terminam com 123.*
   });
 });
