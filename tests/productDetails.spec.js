d3b6421192a3332af263e5ffb6736bb4bf152efe
Dieghonm <dieghonm@gmail.com>
2021-03-28 00:30:48 -0300

    Requisito 6, tentativa 2

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,13 +33,13 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-  const itens = productDetails('Alcool gel', 'Máscara');
-  assert.strictEqual(Array.isArray(itens), true);
-  assert.strictEqual(itens.length, 2);
-  assert.strictEqual(typeof itens[0], typeof itens[1], ['object', 'object']);
-  assert.notStrictEqual(itens[0], itens[1]);
-  assert.strictEqual(itens[0].details.productId
-    .substr(-3), itens[0].details.productId
-    .substr(-3),[123, 123]);
+    const itens = productDetails('Alcool gel', 'Máscara');
+    assert.strictEqual(Array.isArray(itens), true);
+    assert.strictEqual(itens.length, 2);
+    assert.strictEqual(typeof itens[0], typeof itens[1], ['object', 'object']);
+    assert.notStrictEqual(itens[0], itens[1]);
+    assert.strictEqual(itens[0].details.productId
+      .substr(-3), itens[0].details.productId
+      .substr(-3),[123, 123]);
   });
 });
