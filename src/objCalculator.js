facacd4531d65895effd186ae896f6d466a21b41
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-28 03:35:49 -0300

    ajuste

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -1,8 +1,8 @@
 const calculator = {
-  add: (first, second) => { Math.floor(first + second); },
-  mult: (first, second) => { Math.floor(first * second); },
-  divisor: (first, second) => { Math.floor(first / second); },
-  subtra: (first, second) => { Math.floor(first - second); },
-};
+  add: (first, second) => Math.floor(first + second),
+  mult: (first, second) => Math.floor(first * second),
+  div: (first, second) => Math.floor(first / second),
+  sub: (first, second) => Math.floor(first - second),
+}
 
 module.exports = calculator;
