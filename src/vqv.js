5a15f7d77c2595b8afb69ea66e4ed1dc4977f16b
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-28 00:44:00 -0300

    ultimo ajuste da 3 questão. aleluia

--- src/vqv.js
+++ src/vqv.js
@@ -1,6 +1,6 @@
 const vqv = (nome, idade) => {
   if (nome !== undefined && idade !== undefined) {
-    return `Oi, meu é ${nome}!
+    return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
