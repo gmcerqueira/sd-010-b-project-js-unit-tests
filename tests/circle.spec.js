ae43ca3c49f3496737ccfa34d97e90a461774cb7
root <diegoserafim1@gmail.com>
2021-03-27 21:42:48 -0300

    verificando se removi o .fail

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,7 +28,7 @@ describe('#circle', () => {
     // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.strictEqual(circle());
+    assert.strictEqual(circle([]));
     // Teste se o objeto retornado tem 3 entradas.
     assert.strictEqual(circle(), undefined, '');
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
