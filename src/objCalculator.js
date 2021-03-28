7504b0126e63f3f88810ccc8190d3e7a1fff1f7d
Camila <camilafabiabh9@gmail.com>
2021-03-28 16:25:21 -0300

    atualizando

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,22 +20,14 @@
 */
 
 const calculator = {
-  add: (num1, num2) => {
-    const soma = num1 + num2;
-    return parseInt(soma);
-  },
-  mult: (num1, num2) => {
-    const multi = num1 * num2;
-    return parseInt(multi);
-  },
-  div: (num1, num2) => {
-    const divi = num1 / num2;
-    return Math.trunc(divi);
-  },
-  sub: (num1, num2) => {
-    const subt = num1 - num2;
-    return parseInt(subt);
-  },
+  add: (num1, num2) => 
+  parseInt(num1 + num2),
+  mult: (num1, num2) => 
+  parseInt(num1 * num2),
+  div: (num1, num2) => 
+  Math.trunc(num1 / num2),
+  sub: (num1, num2) => 
+  parseInt(num1 - num2),
 };
 
 module.exports = calculator;
