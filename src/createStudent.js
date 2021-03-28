8673ab72edac788a6a33c00ee25c03ef17da9935
Dieghonm <dieghonm@gmail.com>
2021-03-27 21:22:05 -0300

    Requisito 3, tentativa 4

--- src/createStudent.js
+++ src/createStudent.js
@@ -4,7 +4,7 @@
     (1) name, contendo o nome passado como parâmetro;
     (2) feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
 
-  Faça a função da chave feedback com arrow functions!
+  Faça a função da chave  feedback com arrow functions!
 
   Parâmetros:
     - Uma string;
@@ -16,7 +16,7 @@
 */
 const createStudent = (nome) => {
   const estudante = { name: nome,
-    feedback: () => message,
+    feedback: () => 'Eita pessoa boa!',
   };
   return estudante;
 };
