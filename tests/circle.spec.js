927074a8939ecf7d8226b92ca066e5bac94550c2
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-28 02:01:41 -0300

    ajuste

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -6,8 +6,8 @@ describe('#circle', () => {
     assert.strictEqual(typeof (circle(1)), 'object');
     assert.strictEqual(Object.entries(circle(1)).length, 3);
     assert.strictEqual(circle(), undefined);
-    // Pegando apenas o value assert.deepStrictEqual(circle(2)[Object.v(circle(2))[2]], { circumference: 12.56 });
     assert.deepStrictEqual(circle(2), { radius: 2, area: 12.56, circumference: 12.56 });
+    // Pegando apenas o value assert.deepStrictEqual(circle(2)[Object.v(circle(2))[2]], { circumference: 12.56 });
     assert.deepStrictEqual(circle(3)[Object.keys(circle(3))[1]], 28.26);
     assert.deepStrictEqual(circle(3), { radius: 3, area: 28.26, circumference: 18.84 });
   });
