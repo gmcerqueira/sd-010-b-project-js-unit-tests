895771a465dd1c8ee37a59338118461443a69ba5
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-28 03:15:11 -0300

    adicionando a calculadora

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -1,24 +1,10 @@
-/*
-  Desenvolva um objeto calculator que possui quatro chaves:
-    - add;
-    - mult;
-    - div;
-    - sub.
-  Para cada uma delas atribua uma função que realiza a respectiva operação.
-  A função deve receber dois inteiros e retornar um inteiro.
-  Os resultados das divisões devem sempre ser arredondados para baixo.
+const assert = require('assert');
 
-  Faça as funções com arrow functions!
-
-  Parâmetros:
-  - Um número inteiro;
-  - Um número inteiro;
-
-  Comportamento:
-  calculator.add(1, 1) // Retorno: 2;
-  calculator.div(3, 2) // Retorno: 1;
-*/
-
-const calculator = {};
+const calculator = {
+  add: (first, second) => { parseFloat(first + second); },
+  mult: (first, second) => { parseFloat(first * second); },
+  divisor: (first, second) => { parseFloat(first / second); },
+  subtra: (first, second) => { parseFloat(first - second); },
+};
 
 module.exports = calculator;
