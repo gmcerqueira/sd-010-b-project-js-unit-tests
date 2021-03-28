3b648971efd833ff630a6f6fe1d7cf6f0e3d2884
renatogsouza <renatomacedo1995@gmail.com>
2021-03-27 21:05:48 -0300

    correçao lint

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,10 +16,11 @@
 */
 
 const createStudent = (nome) => {
-  return {
+  const estudante = {
     name: nome,
     feedback: _ => 'Eita pessoa boa!',
   };
+  return estudante;
 };
 
 module.exports = createStudent;
