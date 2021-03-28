e537f96f2cd9ce94136a7cfbb98e8f8cc123e3ba
root <diegoserafim1@gmail.com>
2021-03-28 18:02:30 -0300

    criada a funçao objCalculator com o Math.trunc() para arrendondar o resultado

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,13 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/trunc
+  // O método Math.trunc() retorna a parte inteira de um número, descartando suas casas decimais.
+  add: (add1, add2) => Math.trunc(add1 + add2),
+  mult: (mult1, mult2) => Math.trunc(mult1 * mult2),
+  div: (div1, div2) => Math.trunc(div1 / div2),
+  sub: (sub1, sub2) => Math.trunc(sub1 - sub2),
+};
 
 module.exports = calculator;
