6fd1621b9eccdd298ef7ea48a69f6ad3fa96042c
Carlos <cemargato@gmail.com>
2021-03-27 22:47:54 -0300

    acertando requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,12 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => {
-  const estudante = {
+const createStudent = (nome) => ({
     name: nome,
     feedback: () => 'Eita pessoa boa!',
-  };
-  return estudante;
-};
+});
 
 module.exports = createStudent;
