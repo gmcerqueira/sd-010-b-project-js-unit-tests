c143f4ece22ff230049cc8b209850d5ce3db7327
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-28 10:10:05 -0300

    finalmente a 7 erros slint 3

--- src/vqv.js
+++ src/vqv.js
@@ -21,7 +21,7 @@ const vqv = (nome, idade) => {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,`
-  +'\ntrabalho na Trybe e mando muito em programação!\n#VQV!';
+  + '\ntrabalho na Trybe e mando muito em programação!\n#VQV!';
 };
 
 module.exports = vqv;
