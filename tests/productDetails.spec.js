de93f26b88aa0f1eab148db1404cf5d17ab48326
Camila <camilafabiabh9@gmail.com>
2021-03-28 10:46:11 -0300

    atualizando eslint

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -39,13 +39,12 @@ describe('#productDetails', () => {
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails().length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(productDetails()[0], 'object');
-    assert.strictEqual(productDetails()[1], 'object');
+    assert.strictEqual(typeof productDetails(), 'object');
     // Teste que os dois objetos são diferentes entre si.
     assert.strictEqual(productDetails()[0] !== productDetails()[1], true);
     // (Difícil) Teste que os dois productIds terminam com 123.
     assert.strictEqual(productDetails()[0].details.productId.includes('123'), true);
     assert.strictEqual(productDetails()[1].details.productId.includes('123'), true);
     // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
-  });
+  }); 
 });
