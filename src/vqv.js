233c563e4dbe9591e5be5a4ce96c8010f8824513
Breno Rodrigues <brenorm@gmail.com>
2021-03-29 11:59:44 -0300

    Requisito 3: Inclusao de referencia

--- src/vqv.js
+++ src/vqv.js
@@ -20,6 +20,7 @@ const vqv = (nome, idade) => {
   const nameTryber = nome;
   const ageTryber = idade;
 
+  // Referência utilizada para verificar se parâmetros não estão vazios: https://github.com/tryber/sd-010-b-project-js-unit-tests/blob/thiago-marchini-project-js-units-tests/src/vqv.js
   if (!nameTryber && !ageTryber) { return undefined; }
   return `Oi, meu nome é ${nameTryber}!
 Tenho ${ageTryber} anos,
