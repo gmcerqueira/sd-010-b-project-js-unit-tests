1f58735902c7b6d777d17de1f4fea25924b7e984
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-28 21:14:35 -0300

    mycounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,13 +19,11 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let contador = 2; contador <= 3; contador += 1) {
+      myArray.push(contador);
     }
   }
   return myArray;
 };
-
-console.log(myCounter());
 
 module.exports = myCounter;
