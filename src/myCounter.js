419a6f765b692b700064c8f3e986a50ad6050c34
Abimael <abimaelr.albu@gmail.com>
2021-03-29 13:15:44 -0300

    My counter space

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  let myArray = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+  let myArray = [];
+
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
       myArray.push(counter2);
     }
   }
-  myArray = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
   return myArray;
 };
 
