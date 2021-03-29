4e5ca8955ff71529ec60f4bc26917d9779a34c25
Abimael <abimaelr.albu@gmail.com>
2021-03-29 12:52:21 -0300

    My counter corr

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,14 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter2);
-    }
-  }
-  return [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+  let myArray = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+  // for (let counter = 0; counter <= 3; counter += 1) {
+  //   myArray.push(counter);
+  //   for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+  //     myArray.push(counter2);
+  //   }
+  // }
+  return myArray;
 };
 
 module.exports = myCounter;
