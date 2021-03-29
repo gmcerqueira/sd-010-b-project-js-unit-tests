c4af8458e45c38f82799b36f4c6ab2dc941f2cf0
GuilhermeCaixeta17 <guilhermev795@gmail.com>
2021-03-29 14:36:58 -0300

    tentando resolvar o avaluator

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,7 +14,7 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let count = 2; counter <= 3; count += 1) {
+    for (let count = 2; count <= 3; count += 1) {
       myArray.push(count);
     }
   }
