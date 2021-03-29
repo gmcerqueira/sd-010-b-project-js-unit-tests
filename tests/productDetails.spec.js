12b0f32bedee98fb9f9aec436c3c1655252bd4a6
RafaMaths <rmathscoach@gmail.com>
2021-03-29 19:01:19 -0300

    cumpre 4 testes do productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,11 +37,11 @@ describe('#productDetails', () => {
     // Teste que o retorno da função é um array.
     assert.strictEqual(Array.isArray([productDetails]), true); /* Fonte de "Array.isArray": https://pt.stackoverflow.com/questions/77190/como-saber-se-um-objeto-%C3%A9-um-array-em-javascript-sem-jquery#:~:text=O%20m%C3%A9todo%20Array.,e%20false%20se%20n%C3%A3o%20%C3%A9.&text=O%20typeof%20%C3%A9%20um%20Operador,o%20tipo%20de%20um%20Operando. */
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual()
+    assert.strictEqual(productDetails(' ', ' ').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual()
+    assert.strictEqual(typeof productDetails([0]), typeof productDetails([1]), Object);
     // Teste que os dois objetos são diferentes entre si.
-
+    assert.notDeepStrictEqual(productDetails([0]), productDetails([1]), true);
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
