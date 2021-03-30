0f8b3dce8538f8fc084158430203183500eba53f
tomsantos07 <xotmsantosx@live.com>
2021-03-29 23:15:50 -0300

    exercicio #VQV!

--- src/vqv.js
+++ src/vqv.js
@@ -17,15 +17,14 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof nome !== 'string') {
+  if (typeof nome !== 'string' || (typeof idade !== 'number')) {
     return undefined;
-  } else if (typeof idade !== 'number') {
-    return undefined;
-  };
-  return `Oi, meu nome é ${nome}!
+  } else {
+    return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`
+  }
 };
 
 module.exports = vqv;
