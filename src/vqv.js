94c31c3f16b62f8d3c73c90251f858b6ad428d3e
tomsantos07 <xotmsantosx@live.com>
2021-03-29 23:53:50 -0300

    trabalhando os templates

--- src/vqv.js
+++ src/vqv.js
@@ -19,10 +19,8 @@
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
-  } return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  } return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\n`
+  + 'trabalho na Trybe e mando muito em programação!\n#VQV!';
 };
 
 module.exports = vqv;
