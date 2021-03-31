7e7292b16fa9c0f019fa55b38269f73817f0ec70
tomsantos07 <xotmsantosx@live.com>
2021-03-31 01:21:44 -0300

    requisito 4 ajustes

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,23 +27,32 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.deepStrictEqual(typeof (circle(1), 'object'));
 
-    // Teste se o objeto retornado tem 3 entradas.
+    assert.deepStrictEqual(typeof (circle(1)), 'object');
     assert.deepStrictEqual(Object.keys(circle(1)).length, 3);
-
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.deepStrictEqual(circle(), undefined);
-
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.deepStrictEqual(circle(1), { radius: 1, area: 3.14, circumference: 6.28 });
-
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(circle(2), { radius: 2, area: 12.56, circumference: 12.56 });
+    assert.deepStrictEqual(circle(1), {
+      radius: 1,
+      area: 3.14,
+      circumference: 6.28
+    });
+    assert.deepStrictEqual(circle(2), {
+      radius: 2,
+      area: 12.56,
+      circumference: 12.56
+    });
     let a = 3.14 * 3 * 3;
     let c = 3.14 * 3 * 2;
+    assert.deepStrictEqual(circle(3), {
+      radius: 3,
+      area: a,
+      circumference: c
+    });
 
+    // Teste se o objeto retornado tem 3 entradas.
+    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(3), { radius: 3, area: a, circumference: c });
   });
 });
