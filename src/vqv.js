421616f7630a8e9ebc2cfbf10891856b8e2e3e0f
Malbathan Lima <malbathanlima@gmail.com>
2021-03-31 09:45:35 -0300

    Lint Correction

--- src/vqv.js
+++ src/vqv.js
@@ -20,12 +20,12 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  return ( 
+  return (
     `Oi, meu nome é ${nome}!\n`
     + `Tenho ${idade} anos,\n`
     + 'trabalho na Trybe e mando muito em programação!\n'
     + '#VQV!'
-  )
+  );
 };
 
 module.exports = vqv;
