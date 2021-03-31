f66b4f564321652e2e0f02a8abea382c838d1e12
Daniel Ceci <danielceci96@gmail.com>
2021-03-31 20:12:04 -0300

    acho que entendi qual era o erro absurdo que eu tinha cometido

--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,7 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  return `Oi,meu nome é ${nome}!\n Tenho ${idade} anos!\n trabalho na Trybe e mando muito em programação! \n#VQV!`;
+  return `Oi,meu nome é ${nome}blablabla`;
 };
 
 module.exports = vqv;
