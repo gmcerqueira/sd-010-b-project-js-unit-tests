ceb1cbc40e99cd512d2a4ceb6c45b24a86cbe2de
aline <aline.melryaa@gmail.com>
2021-03-31 19:25:23 -0400

    eslint

--- src/vqv.js
+++ src/vqv.js
@@ -18,10 +18,10 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  let str = `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`;
+  let str = `Oi, meu nome é ${nome}!´
+    ´Tenho ${idade} anos,´
+    ´trabalho na Trybe e mando muito em programação!´
+    ´#VQV!`;
 
   return str;
 };
