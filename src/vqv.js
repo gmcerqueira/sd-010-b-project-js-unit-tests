c3a445ec9549bf5a3f6c430dc0e496dde9faba2c
Guilherme Lima <guilhermelimasky@gmail.com>
2021-03-31 14:08:24 -0300

    Corrigindo lint again

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,10 @@
 */
 
 const vqv = (nome, idade) => {
-  let frase = `Oi, meu nome é ${nome}!`
-    `Tenho ${idade} anos,`
-    `trabalho na Trybe e mando muito em programação!`
-    `#VQV!`;
+  let frase = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
   if (nome !== undefined && idade !== undefined) {
     return frase;
   }
