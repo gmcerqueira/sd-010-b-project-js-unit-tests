cf5dcd21417d4bcfad8b06a254f21c68d9142bc6
Guilherme Lima <guilhermelimasky@gmail.com>
2021-03-31 15:06:14 -0300

    requisito 5 corrigindo lint4

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -40,7 +40,7 @@ describe('#productDetails', () => {
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
-    assert.strictEqual(typeof productDetails('Álcool', 'Gel'), 'object' );
+    assert.strictEqual(typeof productDetails('Álcool', 'Gel'), 'object');
     assert.strictEqual(productDetails('teste1', 'teste2').length, 2);
     let produto = productDetails('teste1', 'teste2');
     assert.strictEqual(typeof produto[0], 'object');
