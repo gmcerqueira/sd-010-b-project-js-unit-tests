39a59638a04a22042de249f1db73adcc68a25ae3
Aladino Borges <eualadino@gmail.com>
2021-04-01 15:22:30 -0300

    Requisito 3: refatoração.

--- src/vqv.js
+++ src/vqv.js
@@ -19,7 +19,10 @@
 const vqv = (nome, idade) => {
   if (!nome || !idade) return undefined;
 
-  const message = (`Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`);
+  const message = (`Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`);
 
   return message;
 };
