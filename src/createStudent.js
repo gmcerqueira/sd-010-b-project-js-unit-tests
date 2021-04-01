2aed405162c00d29d217c7124d4fdb9652ba4cf9
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-01 03:52:22 -0300

    Corrigindo a dificuldade foi no createStudents

--- src/createStudent.js
+++ src/createStudent.js
@@ -4,7 +4,7 @@
     (1) name, contendo o nome passado como parâmetro;
     (2) feedback, contendo uma função que retorna a frase 'Eita pessoa boa!' ao ser chamada.
 
-  Faça a função da chave feedback com arrow functions!
+  Faça a função da chae feedback com arrow functions!
 
   Parâmetros:
     - Uma string;
