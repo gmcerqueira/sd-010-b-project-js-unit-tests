33002f62ce05623004de25de59400c94afc7ed9d
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-01 04:55:39 -0300

    nova alteracao em circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -42,7 +42,8 @@ describe('#circle', () => {
     const m = 2 * 3.14 * 3;
     assert.deepStrictEqual(circle(3).area, (3.14 * 3 * 3));
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    const n = (2 * 3.14 * 3);
-    assert.deepStrictEqual(circle(3), { radius: 3, circumference: n.toString(), area: 3.14 * 3 * 3 });
+    const n = (2 * 3.14 * 3).toString();
+    // n.toString();
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 3.14 * 3 * 3, circumference: n });
   });
 });
