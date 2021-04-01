bfb25579de0ae8058fa6b7ba3a5306d3db0f7694
renatogsouza <renatomacedo1995@gmail.com>
2021-04-01 18:20:25 -0300

    requisito restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -79,6 +79,12 @@
 // soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso,
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = (pedido) => ({
+  fetchMenu: () => pedido,
+});
 
+const meuRestaurante = createMenu({ food: { coxinha: 3.9, sopa: 9.9 }, drink: { agua: 3.9, cerveja: 6.9 } });
+const objetoRetornado = createMenu({ food: {}, drink: {} });
+
+console.log(objetoRetornado.fetchMenu());
 module.exports = createMenu;
