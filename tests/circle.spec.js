c17d266d8da58c2384aadb81e8d8eedce7cb34cd
walterlbf <walter.lbf.f@gmail.com>
2021-03-31 22:26:21 -0400

    requisito 4, correcao do acesso ao objeto circle, e criacao do objeto para o circulo de raio 3

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -33,10 +33,14 @@ describe('#circle', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.strictEqual(Object.values(circle(2))[3], 12.56);
+    assert.strictEqual(circle(2).circumference, 12.56);
     // // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(parseFloat(Object.values(circle(3))[1].toPrecision(4)), 28.26);
+    assert.strictEqual(circle(3).area, 28.259999999999998);
     // // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(Object.values(circle(3)), Object.values(circle(3)));
+    assert.deepStrictEqual(circle(3), {
+      radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84,
+    });
   });
 });
