8514028a08174c033d4dbecbcbcd29af3671d814
Daniel Ceci <danielceci96@gmail.com>
2021-04-01 16:27:47 -0300

    requisito product Details

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,21 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    // OBS: O último teste(teste mais difícil) eu só consegui entender a resolução com a ajuda do meu colega Alexandre Damasceno.  Link do PR dele:https://github.com/tryber/sd-010-b-project-js-unit-tests/pull/132/commits/14379b19ba4ce958c6de19905a41d46767c92792
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof (productDetails('firstProduct', 'secondProduct')), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.entries(productDetails('firstProduct', 'secondProduct')).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (productDetails('firstProduct', 'secondProduct')), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails('firstProduct', 'secondProduct'), productDetails('firstProduct', 'secondProduct'));
     // (Difícil) Teste que os dois productIds terminam com 123.
+    let firstProductId = productDetails('firstProduct', 'secondProduct')[0].details.productId.split('');
+    let secondProductId = productDetails('firstProduct', 'secondProduct')[1].details.productId.split('');
+    let firstKeyLastThreeCharacters = `${firstProductId[firstProductId.length - 3]}${firstProductId[firstProductId.length - 2]}${firstProductId[firstProductId.length - 1]}`;
+    let secondKeyLastThreeCharacters = `${secondProductId[secondProductId.length - 3]}${secondProductId[secondProductId.length - 2]}${secondProductId[secondProductId.length - 1]}`;
+    assert.strictEqual(firstKeyLastThreeCharacters === '123' && secondKeyLastThreeCharacters === '123', true);
   });
 });
