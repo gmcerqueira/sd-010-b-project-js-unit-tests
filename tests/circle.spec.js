ef0d5240e626450ef2dfb811732191f23eb5245c
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-04-01 04:06:50 -0300

    Alteraçao ao exercicio circle, mudança de retorno de dados

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -42,7 +42,7 @@ describe('#circle', () => {
     const m = 2 * 3.14 * 3;
     assert.deepStrictEqual(circle(3).area, (3.14 * 3 * 3));
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    const n = parseFloat(2 * 3.14 * 3).toFixed(2);
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 3.14 * 3 * 3, circumference: n });
+    const n = parseFloat(2 * 3.14 * 3).toFixed(2).toString();
+    assert.deepStrictEqual(circle(3), { radius: 3, circumference: n, area: 3.14 * 3 * 3 });
   });
 });
