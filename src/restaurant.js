371b73e93cddef47baa641c4ed61327a4394a71b
Thiago Leite <leite.tjs@gmail.com>
2021-04-01 15:58:15 -0300

    restaurante passo 1

--- src/restaurant.js
+++ src/restaurant.js
@@ -80,8 +80,7 @@
 // você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
 const createMenu = (objeto) => ({
-  fetchMenu: () => {},
- 
+  fetchMenu: () => objeto, 
 });
 
 module.exports = createMenu;
