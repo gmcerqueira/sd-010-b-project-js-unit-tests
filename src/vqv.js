82b863634191159d7ba68b6bb145faa534f3741b
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-04-01 17:30:54 -0300

    request 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const message = `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+  if (nome === '' || idade === '') return undefined;
+  return message;
+};
 
 module.exports = vqv;
