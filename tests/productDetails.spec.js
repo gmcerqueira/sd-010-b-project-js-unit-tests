4c313e4ad4c1e14d4bb7c1d7444ec4a1c671e8bb
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-04-02 15:03:17 -0300

    requisito 6 completo

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -42,6 +42,9 @@ describe('#productDetails', () => {
     assert.strictEqual(typeof productDetails()[0], 'object');
     assert.strictEqual(typeof productDetails()[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails()[0], productDetails()[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails()[0].details.productId.endsWith('123'), true);
+    assert.strictEqual(productDetails()[1].details.productId.endsWith('123'), true);
   });
 });
