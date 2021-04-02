4932aa822d8e5b596c77e37b60a0fab5579b6c3d
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-04-01 22:08:05 -0300

    CREATESTUDENT: tests 5, 6, 7 and 8

--- tests/createStudent.spec.js
+++ tests/createStudent.spec.js
@@ -29,11 +29,11 @@ describe('#createStudent', () => {
     assert.strictEqual(typeof estudante.feedback, 'function');
     assert.strictEqual(estudante.name, 'Leandrão, o Lobo Solitário');
     assert.strictEqual(estudante.feedback(), message);
-    // const estudante2 = createStudent('Nobre');
-    // assert.strictEqual(typeof estudante2, 'object');
-    // assert.strictEqual(typeof estudante2.feedback, 'function');
-    // assert.strictEqual(estudante2.name, 'Nobre');
-    // assert.strictEqual(estudante2.feedback(), message);
+    const estudante2 = createStudent('Nobre');
+    assert.strictEqual(typeof estudante2, 'object');
+    assert.strictEqual(typeof estudante2.feedback, 'function');
+    assert.strictEqual(estudante2.name, 'Nobre');
+    assert.strictEqual(estudante2.feedback(), message);
     // const estudante3 = createStudent('Inácio');
     // assert.strictEqual(typeof estudante3, 'object');
     // assert.strictEqual(typeof estudante3.feedback, 'function');
