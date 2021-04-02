2a357b24863caa9ca7e08fc42f688e2b6f338c87
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-04-02 16:10:00 -0300

    Teste requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => ({
+  name,
+  feedbak: () => 'Eita pessoa boa!',
+});
 
 module.exports = createStudent;
