acd70eb8c3372b74f281a1aebe90ab70164292b8
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-04-02 13:55:57 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const Student = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return Student
+};
 
 module.exports = createStudent;
