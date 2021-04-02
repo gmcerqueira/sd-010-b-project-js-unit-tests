f84f5ca79f8f9b00e35af3e9ac9905e8247a4d74
brunobastosfer <brunobastos@edu.unifor.br>
2021-04-02 02:53:56 -0300

    ajuste circle3

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -9,6 +9,6 @@ describe('#circle', () => {
     assert.deepStrictEqual(circle(2), { radius: 2, area: 12.56, circumference: 12.56 });
     // Pegando apenas o value assert.deepStrictEqual(circle(2)[Object.v(circle(2))[2]], { circumference: 12.56 });
     assert.deepStrictEqual((circle(3)).area, 28.259999999999998);
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.26, circumference: 18.84 });
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 3.14 * 3 * 3, circumference: 18.84 });
   });
 });
