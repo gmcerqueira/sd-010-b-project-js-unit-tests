6122fee48976e9b4006eb2938aa4c7c2f4dfffd6
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-04-02 15:10:35 -0300

    OBJCALCULATOR: test 4

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,13 +20,15 @@
 */
 
 const calculator = {
-  add: (num1, num2) => Math.floor(num1 + num2),
-  mult: (num1, num2) => Math.floor(num1 * num2),
+  add: (num1, num2) => num1 + num2,
+  mult: (num1, num2) => num1 * num2,
   div: (num1, num2) => Math.floor(num1 / num2),
+  sub: (num1, num2) => num1 - num2,
 };
 
-console.log(calculator.add(1, 1));
-console.log(calculator.mult(1, 14));
+console.log(calculator.add(3, 2));
+console.log(calculator.mult(3, 2));
 console.log(calculator.div(3, 2));
+console.log(calculator.sub(1, 3));
 
 module.exports = calculator;
