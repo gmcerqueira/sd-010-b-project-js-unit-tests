f8185f9914919e34e3aa9d988f61fcc2d31ce680
Lara Capila <laracapilak@gmail.com>
2021-04-02 20:51:49 -0300

    requisito circle e productsDetails

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -39,7 +39,7 @@ describe('#circle', () => {
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(
       circle(3),
-      { radius: 3, area: 28.259999999999998, circumference: 18.84 }
+      { radius: 3, area: 28.259999999999998, circumference: 18.84 },
     );
   });
 });
