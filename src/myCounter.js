694f47bd6c99efd74de6f022f136629ff8e018b0
Michelle <mls.srbek@gmail.com>
2021-04-03 03:55:12 -0300

    trying to fix conflict

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,7 +16,7 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (var counter = 2; counter <= 3; counter += 1) {
