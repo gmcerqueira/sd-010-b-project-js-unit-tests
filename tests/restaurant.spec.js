85a52754620779ab50029e23eed9d6d945705bf0
Jander Liborio <jandernunes14@gmail.com>
2021-04-02 20:43:19 -0400

    feito os teste do circle

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -57,12 +57,19 @@ describe('#createMenu', () => {
     // ```
     // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
     // ```
+    assert.strictEqual(typeof (createMenu()), 'object');
     // TESTE 2: Verifique que, dado que a função createMenu foi chamada com o objeto: `{ food: {}, drink: {} }`,
     // verifique que 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`.
     // ```
     // const objetoRetornado = createMenu({ food: {}, drink: {} });
     // objetoRetornado.fetchMenu() // Retorno: { food: {}, drink: {}}
     // ```
+    const objetoRetornado = createMenu({
+      food: {},
+      drink: {}
+    });
+    const stringFoodDrink = Object.keys(objetoRetornado.fetchMenu());
+    assert.deepStrictEqual(stringFoodDrink, ['food', 'drink']);
     // TESTE 3: Verifique que o menu passado pra função createMenu é identico ao menu recuperado pela função 'objetoRetornado.fetchMenu'
     // ```
     // const objetoRetornado = createMenu(objetoQualquer);
