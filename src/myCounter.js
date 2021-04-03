fb46171bae969a3106499d59dfd27b1f19b33eba
Michelle <mls.srbek@gmail.com>
2021-04-03 12:11:59 -0300

    myCounter function

--- src/myCounter.js
+++ src/myCounter.js
@@ -11,16 +11,11 @@
 */
 
 const myCounter = () => {
-<<<<<<< HEAD
   const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
-=======
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
->>>>>>> c9ec41cf276637cebc086a1aa3f5386e31e72c0d
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
