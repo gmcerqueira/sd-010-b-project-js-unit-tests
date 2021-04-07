79011e32602bb05afff7d2ed9ab49ea62ecf8a30
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-04-07 15:35:59 -0300

    exercicio circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -42,8 +42,7 @@ describe('#circle', () => {
     assert.strictEqual(Math.round(circle(3).area), 28);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
 
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998,
-      circumference: 18.84 });
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 3.14 * 3 * 3, circumference: 18.84 });
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
