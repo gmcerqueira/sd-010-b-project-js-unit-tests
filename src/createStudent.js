e9b22b3dd64e2bc2ee723ba321d9e95a4b075653
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-04-07 17:21:33 -0300

    vamoss

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,9 +16,10 @@
 */
 
 const createStudent = (nome) => {
-  return { name: nome,
+  const estudante = { name: nome,
     feedback: () => 'Eita pessoa boa!',
   };
+  return estudante;
 };
 
 module.exports = createStudent;
