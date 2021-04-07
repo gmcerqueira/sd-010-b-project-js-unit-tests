30c6a7156144dc3b771ba89f4b18f84b9e88cd02
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-04-07 17:01:42 -0300

    alteraçoes vqv

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -26,6 +26,8 @@ describe('#numbers', () => {
 
     assert.strictEqual(numbers([1, 'a', 3]), false);
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+
+    assert.strictEqual(numbers(['']), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
   });
 });
